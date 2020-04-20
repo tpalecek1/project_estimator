cd1fc04e616cbc4e0b5316d9706415c5717c8c05
cweng777 <cweng777@gmail.com>
2020-04-20 22:38:26 +0800

    update project list layout-search bar, filter icons

--- lib/screens/project_list.dart
+++ lib/screens/project_list.dart
@@ -4,7 +4,14 @@ import 'package:project_estimator/screens/user_setting.dart';
 
 import 'edit_project.dart';
 
-class ProjectList extends StatelessWidget {
+class ProjectList extends StatefulWidget {
+  @override
+  _ProjectListState createState() => _ProjectListState();
+}
+
+class _ProjectListState extends State<ProjectList> {
+  TextEditingController controller = TextEditingController();
+  bool showCancel = false;
   //fake list data
   final items = List<Map>.generate(100, (i) {
     return {
@@ -13,6 +20,7 @@ class ProjectList extends StatelessWidget {
       'category':'category $i'
     };
   });
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -29,24 +37,95 @@ class ProjectList extends StatelessWidget {
       ),
       body: Column(
         children: [
-          Flexible(
-            flex: 1,
+          SizedBox(
+            height: 50,
             child: Row(children: [
-              Flexible(child: Padding(
+              Expanded(child: Padding(
+                padding: const EdgeInsets.only(left: 8, right: 4, top: 4, bottom: 4),
+                child: Card(
+                  shape: RoundedRectangleBorder(
+                    side: BorderSide(color: Colors.blue, width: 1),
+                    borderRadius: BorderRadius.circular(15.0),
+                  ),
+                  child: Row(
+                    children:[
+                      Expanded(
+                        child: TextField(
+                          controller: controller,
+                          decoration: InputDecoration(
+                            hintText: 'Search', border: InputBorder.none, contentPadding: const EdgeInsets.only(left: 12, bottom: 12)
+                          ),
+                          onChanged: onSearchTextChanged,
+                        ),
+                      ),
+                      Visibility(
+                        visible: showCancel,
+                        child: IconButton(
+                          padding: EdgeInsets.only(bottom: 2),
+                          color: Colors.blue,
+                          icon: Icon(Icons.cancel), 
+                          onPressed: () {
+                            controller.clear();
+                            showCancel = false;
+                            setState((){});
+                        })
+                      ),
+                    ]
+                  ),
+                ),
+              )),
+              Padding(
                 padding: const EdgeInsets.all(8.0),
-                child: Placeholder(),
-              ), flex: 3),
-              Flexible(child: Padding(
+                child: Material(
+                  child: Ink(
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Colors.black, width: 1.0),
+                      color: Colors.white,
+                      shape: BoxShape.rectangle,
+                    ),
+                    child: InkWell(
+                      //splashColor: Colors.lightBlueAccent,
+                      onTap: (){},
+                      child: SizedBox(
+                        width: 50,
+                        child: Icon(
+                            Icons.search,
+                            size: 30.0,
+                            color: Colors.black,
+                        ),
+                      ),                    
+                    ),
+                  )
+                )
+              ),
+              Padding(
                 padding: const EdgeInsets.all(8.0),
-                child: Placeholder(),
-              ), flex: 1),
-              Flexible(child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Placeholder(),
-              ), flex: 1),
-            ])),
-          Flexible(
-            flex: 9,
+                child: Material(
+                  child: Ink(
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Colors.black, width: 1.0),
+                      color: Colors.white,
+                      shape: BoxShape.rectangle,
+                    ),
+                    child: InkWell(
+                      //splashColor: Colors.lightBlueAccent,
+                      onTap: (){},
+                      child: SizedBox(
+                        width: 50,
+                        child: Icon(
+                            Icons.filter_list,
+                            size: 30.0,
+                            color: Colors.black,
+                        ),
+                      ),                    
+                    ),
+                  )
+                )
+              ),
+            ])
+          ),
+          Divider(height: 1),
+          Expanded(
             child: ListView.builder(
               itemCount: items.length,
               itemBuilder: (context, index) {
@@ -71,6 +150,17 @@ class ProjectList extends StatelessWidget {
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat
     );
-    
+  }
+  void onSearchTextChanged(String text) {
+    if(text.isEmpty) {
+      showCancel = false;
+      setState(() {});
+      return;
+    } else {
+      showCancel = true;
+      setState(() {});
+      return;
+    }
   }
 }
+
