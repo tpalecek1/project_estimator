c185ae0b79d01117f669ac59f97e98a2835759ec
cweng777 <cweng777@gmail.com>
2020-04-25 21:42:55 +0800

    update ProjectNote screen

--- lib/screens/project_notes.dart
+++ lib/screens/project_notes.dart
@@ -1,9 +1,41 @@
 import 'package:flutter/material.dart';
 
-class ProjectNotes extends StatelessWidget {
+class ProjectNotes extends StatefulWidget {
 
   static const routeName = 'project_notes';
 
+  @override
+  _ProjectNotesState createState() => _ProjectNotesState();
+}
+
+class _ProjectNotesState extends State<ProjectNotes> {
+
+  // The entire multilevel list displayed by this app. (fake data)
+  List<Entry> data = <Entry>[
+    Entry('General Notes',
+      <Entry>[
+        Entry('General Note 1 long long long long long long long long long long long long long long long long note'),
+        Entry('General Note 2'),
+        Entry('General Note 3'),
+      ],
+    ),
+    Entry('Room Notes',
+      <Entry>[
+        Entry('Room #1 Name',
+          <Entry>[
+            Entry('Room #1 Note 1',[],[{'key': 'object A', 'value': 224}, {'key': 'object B', 'value': 12.12}]),
+            Entry('Room #1 Note 2'),
+          ],
+        ),
+        Entry('Room #2 Name',
+          <Entry>[
+            Entry('Room #2 Note 1'),
+            Entry('Room #2 Note 2',[],[{'key': 'object E', 'value': 20}, {'key': 'object F', 'value': 60}]),
+          ],
+        ),
+      ],
+    ),
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,12 +51,26 @@ class ProjectNotes extends StatelessWidget {
             padding: EdgeInsets.only(top: 16),
             itemCount: data.length,
             itemBuilder: (context, index) {
-              return EntryItem(data[index]);
+              return EntryItem(data[index], index, delete);
             }
           ),
         ],
       )
     );
+  }
+
+  //the delete function
+  void delete(int index, int roomIndex, int noteIndex) {
+    setState(() {
+      if(index == 0) {
+        data[index].children.removeAt(noteIndex);
+      } else {
+        data[index].children[roomIndex].children.removeAt(noteIndex);
+        if(data[index].children[roomIndex].children.isEmpty) {
+          data[index].children.removeAt(roomIndex);
+        }
+      }
+    });
   }
 }
 
@@ -34,43 +80,26 @@ class Entry {
   final String title;
   final List<Entry> children;
   final List<Map<String,dynamic>> estimateItem;
+  int roomIndex = -99; //for tracking room index
+  int noteIndex = -99; //for tracking each note item
 }
-
-// The entire multilevel list displayed by this app. (fake data)
-final List<Entry> data = <Entry>[
-  Entry('General Notes',
-    <Entry>[
-      Entry('General Note 1 long long long long long long long long long long long long long long long long note'),
-      Entry('General Note 2'),
-      Entry('General Note 3'),
-    ],
-  ),
-  Entry('Room Notes',
-    <Entry>[
-      Entry('Room #1 Name',
-        <Entry>[
-          Entry('Room #1 Note 1',[],[{'key': 'object A', 'value': 224}, {'key': 'object B', 'value': 12.12}]),
-          Entry('Room #1 Note 2'),
-        ],
-      ),
-      Entry('Room #2 Name',
-        <Entry>[
-          Entry('Room #2 Note 1'),
-          Entry('Room #2 Note 2',[],[{'key': 'object E', 'value': 20}, {'key': 'object F', 'value': 60}]),
-        ],
-      ),
-    ],
-  ),
-];
 
 // Displays one Entry. If the entry has children then it's displayed with an ExpansionTile.
 class EntryItem extends StatelessWidget {
-  const EntryItem(this.entry);
-
+  EntryItem(this.entry, this.index, this.delete);
+  int index;
   final Entry entry;
+  Function(int, int, int) delete;
+
+  //there three kinds of indexes are used to track each item in the data
+  int roomIndex = -2; //for tracking room index
+  int roomIndexKeep = -99; //for helping restarting noteIndex for each room
+  int noteIndex = -1; //for tracking each note item
 
   @override
   Widget build(BuildContext context) {
+    roomIndex = -2;
+    noteIndex = -1;
     return Card(
       shape: RoundedRectangleBorder(
         side: BorderSide(color: Colors.brown, width: 3),
@@ -82,13 +111,40 @@ class EntryItem extends StatelessWidget {
   }
 
   Widget _buildTiles(Entry root) {
+
+    //assign index to each entry, so later each item can be tracked
     if (root.children.isEmpty)
     {
-      if(root.estimateItem.isEmpty)
-        return ListTile(title: Text(root.title));
+      if(index == 0) { //index: 0 means "General Note" area, index:1 means "Room Note" area
+        noteIndex++;
+        root.noteIndex = noteIndex;
+      } else {
+        if(roomIndex != roomIndexKeep) { //if these two type of indexes are not the same, restart the coutn of noteIndex
+          noteIndex = -1;
+          roomIndexKeep = roomIndex;
+        }
+        noteIndex++;
+        root.noteIndex = noteIndex;
+        root.roomIndex = roomIndex;  
+      }
+
+      //Assign layout to each entry object
+      if(root.title == "General Notes" || root.title == "Room Notes") 
+        return ListTile(title: Text(root.title), trailing:Text('empty', style: TextStyle(color: Colors.red)));
+      else if(root.estimateItem.isEmpty)
+      {
+        return ListTile(title: Text(root.title), trailing: RaisedButton(onPressed: () {
+          //print('$index, ${root.roomIndex}, ${root.noteIndex}');
+          delete(index, root.roomIndex, root.noteIndex);
+        }, child: Text('delete')));
+      }
       else
-        return CustomTile(title: Text(root.title), items: root.estimateItem);
+        return CustomTile(title: Text(root.title), items: root.estimateItem, trailing: RaisedButton(onPressed: () {
+          //print('$index, ${root.roomIndex}, ${root.noteIndex}');
+          delete(index, root.roomIndex, root.noteIndex);
+        }, child: Text('delete')));
     }
+    roomIndex++;  //for tracking room index
     return ExpansionTile(
       initiallyExpanded: root.title.contains('Room')? true: false, //expand all Room notes initailly
       key: PageStorageKey<Entry>(root),
@@ -101,31 +157,39 @@ class EntryItem extends StatelessWidget {
 class CustomTile extends StatelessWidget {
   final Widget title;
   final List<Map<String,dynamic>> items;
-  const CustomTile({Key key, this.title, this.items}): super(key: key);
+  final Widget trailing;
+  CustomTile({Key key, this.title, this.items, this.trailing}): super(key: key);
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.only(left: 16, right: 16, bottom: 16),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
+      child: Row(
         children: [
-          AnimatedDefaultTextStyle( //it is used for runtime text change, but here just take advantage of its style change addition
-            style: TextStyle(fontSize: 16, color: Colors.black),
-            duration: kThemeChangeDuration,
-            child: title
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                AnimatedDefaultTextStyle( //it is used for runtime text change, but here just take advantage of its style change addition
+                  style: TextStyle(fontSize: 16, color: Colors.black),
+                  duration: kThemeChangeDuration,
+                  child: title
+                ),
+                Table(
+                  children: items.map<TableRow>(_buildTableRow).toList()
+                  // [
+                  //   TableRow(children: [
+                  //     Text('test1'), Text('test2')
+                  //   ]),
+                  //   TableRow(children: [
+                  //     Text('test3'), Text('test4')
+                  //   ])
+                  // ]
+                )
+              ],
+            ),
           ),
-          Table(
-            children: items.map<TableRow>(_buildTableRow).toList()
-            // [
-            //   TableRow(children: [
-            //     Text('test1'), Text('test2')
-            //   ]),
-            //   TableRow(children: [
-            //     Text('test3'), Text('test4')
-            //   ])
-            // ]
-          )
-        ],
+          Container(child: trailing)
+        ]
       ),
     );
   }
