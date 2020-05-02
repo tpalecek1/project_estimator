ece43295d5b8fa579bb4e72c9bd6e1a7f041ef20
tpalecek1 <36023933+tpalecek1@users.noreply.github.com>
2020-05-02 10:16:23 -0700

    Added detail screens

--- lib/screens/project_detail.dart
+++ lib/screens/project_detail.dart
@@ -4,37 +4,144 @@ import 'package:project_estimator/screens/project_estimate.dart';
 import 'package:project_estimator/screens/project_notes.dart';
 import 'package:project_estimator/screens/room_detail.dart';
 import 'package:project_estimator/widgets/custom_button_1.dart';
+import '../models/project.dart';
+import '../models/room.dart';
 
-class ProjectDetail extends StatelessWidget {
-
+class ProjectDetail extends StatefulWidget{
+  ProjectDetail({Key key}) : super(key: key);
   static const routeName = 'project_detail';
 
-  //fake room data
-  final rooms = [{'name': 'Living Room'},
-                 {'name': 'BedRoom'},
-                 {'name': 'Kitchen'},
-                 {'name': 'Master Bathroom'}];
+  @override
+  _ProjectDetailState createState() => _ProjectDetailState();
+}
+
+
+class _ProjectDetailState extends State<ProjectDetail> {
+
+  Project project;
+  List<Room> rooms = [];
+
+  @override 
+  void initState() { 
+    super.initState();
+    //todo - Get project and rooms(only name and ID needed for rooms) from database if exists, get project's rooms, fill rooms list
+    getData();
+  }
+
+  void getData() async {
+    await Future.delayed(const Duration(seconds: 1)); //Simulating delay from getting data
+    //If project does not exist in database, create new project
+    getFakeData();
+    setState((){});
+  }
+  
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return waitData();
+  }
+
+  Widget waitData(){
+    if(project == null){
+      return Center(
+        child: CircularProgressIndicator()
+      );
+    }
+    else{
+      return Scaffold(
       appBar: AppBar(
-        title: Text('Project Name'),
-        actions: [
-          Center(child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: SizedBox(child: Placeholder(), width: 100),
-          ))
-        ],
+        title: Text(project.name),
       ),
       body: Column(
         children: [
-          Flexible(
-            flex: 2,
-            child: Placeholder()
+          Text(
+            'Project Description',
+            style: Theme.of(context).textTheme.display1,
+            textAlign: TextAlign.left,
           ),
           Flexible(
-            flex: 2,
+            flex: 3,
+            child: Container(
+              margin: EdgeInsets.all(3),
+              decoration: BoxDecoration(border: Border.all(width: 1.2), borderRadius: BorderRadius.circular(10)),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  ListTile(
+                    dense: true,
+                    title: Row(
+                      children:[
+                        Text(
+                          'Customer Name:', 
+                          style: TextStyle(
+                            fontSize: 20, 
+                            fontWeight: FontWeight.w400,
+                            decoration: TextDecoration.underline,
+                          )
+                        ),
+                        Padding(padding: EdgeInsets.symmetric(horizontal: 3)),
+                        Text(
+                          project.clientName, 
+                          style: TextStyle(
+                            fontSize: 20, 
+                            fontWeight: FontWeight.w400,
+                          )
+                        ),
+                      ]
+                      ),
+                  ),
+                  ListTile(
+                    dense: true,
+                    title: Row(
+                      children:[
+                        Text(
+                          'Address:', 
+                          style: TextStyle(
+                            fontSize: 20, 
+                            fontWeight: FontWeight.w400,
+                            decoration: TextDecoration.underline,
+                          )
+                        ),
+                        Padding(padding: EdgeInsets.symmetric(horizontal: 3)),
+                        Text(
+                          project.clientAddress, 
+                          style: TextStyle(
+                            fontSize: 20, 
+                            fontWeight: FontWeight.w400,
+                          )
+                        ),
+                      ]
+                      ),
+                  ),
+                  ListTile(
+                    dense: true,
+                    title: Row(
+                      children:[
+                        Text(
+                          'Description:', 
+                          style: TextStyle(
+                            fontSize: 20, 
+                            fontWeight: FontWeight.w400,
+                            decoration: TextDecoration.underline,
+                          )
+                        ),
+                        Padding(padding: EdgeInsets.symmetric(horizontal: 3)),
+                        Text(
+                          project.description, 
+                          style: TextStyle(
+                            fontSize: 20, 
+                            fontWeight: FontWeight.w400,
+                          )
+                        ),
+                      ]
+                      ),
+                  ),
+                ],
+              )
+            )
+          ),
+          Flexible(
+            flex: 1,
             fit: FlexFit.tight,
             child: Row(
               children: [
@@ -43,7 +150,7 @@ class ProjectDetail extends StatelessWidget {
                     onPressed: () {
                       Navigator.of(context).pushNamed(EditProject.routeName);
                     },
-                    child: Text('Edit Project Button')
+                    child: Text('Edit Project')
                   )
                 ),
                 Expanded(
@@ -51,7 +158,7 @@ class ProjectDetail extends StatelessWidget {
                     onPressed: () {
                       Navigator.of(context).pushNamed(ProjectNotes.routeName);
                     },
-                    child: Text('Show Project Notes Button')
+                    child: Text('Show Project Notes')
                   )
                 ),
                 Expanded(
@@ -59,20 +166,25 @@ class ProjectDetail extends StatelessWidget {
                     onPressed: () {
                       Navigator.of(context).pushNamed(ProjectEstimate.routeName);
                     },
-                    child: Text('Show Project Estimate Button')
+                    child: Text('Show Project Estimate')
                   )
                 ),
               ],
             ),
           ),
           Flexible(
-            flex: 3,
-            child: Placeholder()
-          ),
-          Flexible(
             flex: 1,
             fit: FlexFit.tight,
-            child: Container(child: Align(child: Text('Rooms'), alignment: Alignment.centerLeft))
+            child: Container(
+              margin: EdgeInsets.only(left: 3),
+              child: Align(
+                child: Text(
+                  'Rooms',
+                  style: Theme.of(context).textTheme.display1
+                ), 
+                alignment: Alignment.centerLeft
+              )
+            )
           ),
           //todo: should be in the list, make fake listview data
           Flexible(
@@ -82,9 +194,9 @@ class ProjectDetail extends StatelessWidget {
               itemBuilder: (context, index) {
                 return Card(
                   child: ListTile(
-                    title: Text('${rooms[index]['name']}'),
+                    title: Text('${rooms[index].name}'),
                     onTap: () {
-                      Navigator.of(context).pushNamed(RoomDetail.routeName);
+                      Navigator.of(context).push(MaterialPageRoute(builder: (context) => RoomDetail(room: rooms[index])));
                     },
                   ),
                 );
@@ -94,5 +206,13 @@ class ProjectDetail extends StatelessWidget {
         ]
       ),
     );
+    }
+  }
+
+  void getFakeData(){
+    project = Project(id: 0, name: 'Painting house', clientName: 'Sherlock', clientAddress: '221B Baker St.', description: 'Painting Exterior');
+    rooms.add(Room(name: 'Kitchen', id: 0, ceilingHeight: 10, length: 12, width: 15, doorCount: 2, windowCount: 4, hasBaseboard: true));
+    rooms.add(Room(name: 'Bedroom', id: 1, ceilingHeight: 12, length: 16, width: 16, doorCount: 1, windowCount: 2, hasBaseboard: true, hasCrown: true));
+    rooms.add(Room(name: 'Bathroom', id: 2, ceilingHeight: 8, length: 11, width: 10, doorCount: 1, windowCount: 0, accentWallCount: 1));
   }
 }
--- lib/screens/room_detail.dart
+++ lib/screens/room_detail.dart
@@ -1,24 +1,323 @@
 import 'package:flutter/material.dart';
 import 'package:project_estimator/screens/room_photo_gallery.dart';
+import '../models/room.dart';
+import '../models/room_note.dart';
+import '../widgets/custom_button_1.dart';
 
-class RoomDetail extends StatelessWidget {
-
+class RoomDetail extends StatefulWidget{
+  RoomDetail({Key key, this.room}) : super(key: key);
   static const routeName = 'room_detail';
+  final Room room;
+
+  @override
+  _RoomDetailState createState() => _RoomDetailState();
+}
+
+
+class _RoomDetailState extends State<RoomDetail> {
+  Room room;
+  List<RoomNote> notes = [];
+
+  @override 
+  void initState() { 
+    super.initState();
+    getData();
+  }
+
+  void getData() async {
+    await Future.delayed(const Duration(seconds: 1)); //Simulating delay from getting data
+    getFakeData();
+    setState((){});
+  }
+  
 
   @override
   Widget build(BuildContext context) {
+    return waitData();
+  }
+
+  Widget waitData() {
+    if(room == null){
+      return Center(
+        child: CircularProgressIndicator()
+      );
+    }
+    else{
     return Scaffold(
       appBar: AppBar(
-        title: Text('xxx Room'),
+        title: Text(room.name),
       ),
-      body: Center(
-        child: RaisedButton(
-          onPressed: () {
-            Navigator.of(context).pushNamed(RoomPhotoGallery.routeName);
-          },
-          child: Text('View Photos'),
-        )
-      )
+      body: Column(
+        children: [
+          Text(
+            'Room Details',
+            style: Theme.of(context).textTheme.display1,
+            textAlign: TextAlign.left,
+          ),
+          Flexible(
+            flex: 3,
+            child: Container(
+              padding: EdgeInsets.all(3),
+              margin: EdgeInsets.all(3),
+              decoration: BoxDecoration(border: Border.all(width: 1.2), borderRadius: BorderRadius.circular(10)),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Column(
+                    mainAxisSize: MainAxisSize.min,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Row(
+                        children:[
+                          Text(
+                            'Ceiling Height:', 
+                            style: TextStyle(
+                              fontSize: 20, 
+                              fontWeight: FontWeight.w400,
+                              decoration: TextDecoration.underline,
+                            )
+                          ),
+                          Padding(padding: EdgeInsets.symmetric(vertical: 15, horizontal: 3)),
+                          Text(
+                            room.ceilingHeight.toString(), 
+                            style: TextStyle(
+                              fontSize: 20, 
+                              fontWeight: FontWeight.w400,
+                            )
+                          ),
+                        ]
+                      ),
+                      Row(
+                        children:[
+                          Text(
+                            'Length:', 
+                            style: TextStyle(
+                              fontSize: 20, 
+                              fontWeight: FontWeight.w400,
+                              decoration: TextDecoration.underline,
+                            )
+                          ),
+                          Padding(padding: EdgeInsets.symmetric(vertical: 15, horizontal: 3)),
+                          Text(
+                            room.length.toString(), 
+                            style: TextStyle(
+                              fontSize: 20, 
+                              fontWeight: FontWeight.w400,
+                            )
+                          ),
+                        ]
+                      ),
+                      Row(
+                        children:[
+                          Text(
+                            'Width:', 
+                            style: TextStyle(
+                              fontSize: 20, 
+                              fontWeight: FontWeight.w400,
+                              decoration: TextDecoration.underline,
+                            )
+                          ),
+                          Padding(padding: EdgeInsets.symmetric(vertical: 15, horizontal: 3)),
+                          Text(
+                            room.width.toString(), 
+                            style: TextStyle(
+                              fontSize: 20, 
+                              fontWeight: FontWeight.w400,
+                            )
+                          ),
+                        ]
+                      ),
+                      Row(
+                        children:[
+                          Text(
+                            'Doors:', 
+                            style: TextStyle(
+                              fontSize: 20, 
+                              fontWeight: FontWeight.w400,
+                              decoration: TextDecoration.underline,
+                            )
+                          ),
+                          Padding(padding: EdgeInsets.symmetric(vertical: 15, horizontal: 3)),
+                          Text(
+                            room.doorCount.toString(), 
+                            style: TextStyle(
+                              fontSize: 20, 
+                              fontWeight: FontWeight.w400,
+                            )
+                          ),
+                        ]
+                      ),
+                      Row(
+                        children:[
+                          Text(
+                            'Windows:', 
+                            style: TextStyle(
+                              fontSize: 20, 
+                              fontWeight: FontWeight.w400,
+                              decoration: TextDecoration.underline,
+                            )
+                          ),
+                          Padding(padding: EdgeInsets.symmetric(vertical: 15, horizontal: 3)),
+                          Text(
+                            room.windowCount.toString(), 
+                            style: TextStyle(
+                              fontSize: 20, 
+                              fontWeight: FontWeight.w400,
+                            )
+                          ),
+                        ]
+                      ),
+                    ],
+                  ),
+                  Padding(padding: EdgeInsets.symmetric(horizontal: 10),),
+                  Column(
+                    mainAxisSize: MainAxisSize.min,
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Row(
+                        children:[
+                          Text(
+                            'Accent Walls:', 
+                            style: TextStyle(
+                              fontSize: 20, 
+                              fontWeight: FontWeight.w400,
+                              decoration: TextDecoration.underline,
+                            )
+                          ),
+                          Padding(padding: EdgeInsets.symmetric(vertical: 15, horizontal: 3)),
+                          Text(
+                            room.accentWallCount.toString(), 
+                            style: TextStyle(
+                              fontSize: 20, 
+                              fontWeight: FontWeight.w400,
+                            )
+                          ),
+                        ]
+                      ),
+                      Row(
+                        children:[
+                          Text(
+                            'Baseboards:', 
+                            style: TextStyle(
+                              fontSize: 20, 
+                              fontWeight: FontWeight.w400,
+                              decoration: TextDecoration.underline,
+                            )
+                          ),
+                          Padding(padding: EdgeInsets.symmetric(vertical: 15, horizontal: 3)),
+                          Text(
+                            room.hasBaseboard.toString(), 
+                            style: TextStyle(
+                              fontSize: 20, 
+                              fontWeight: FontWeight.w400,
+                            )
+                          ),
+                        ]
+                      ),
+                      Row(
+                        children:[
+                          Text(
+                            'Chair Rail:', 
+                            style: TextStyle(
+                              fontSize: 20, 
+                              fontWeight: FontWeight.w400,
+                              decoration: TextDecoration.underline,
+                            )
+                          ),
+                          Padding(padding: EdgeInsets.symmetric(vertical: 15, horizontal: 3)),
+                          Text(
+                            room.hasChairRail.toString(), 
+                            style: TextStyle(
+                              fontSize: 20, 
+                              fontWeight: FontWeight.w400,
+                            )
+                          ),
+                        ]
+                      ),
+                      Row(
+                        children:[
+                          Text(
+                            'Crown Molding:', 
+                            style: TextStyle(
+                              fontSize: 20, 
+                              fontWeight: FontWeight.w400,
+                              decoration: TextDecoration.underline,
+                            )
+                          ),
+                          Padding(padding: EdgeInsets.symmetric(vertical: 15, horizontal: 3)),
+                          Text(
+                            room.hasCrown.toString(), 
+                            style: TextStyle(
+                              fontSize: 20, 
+                              fontWeight: FontWeight.w400,
+                            )
+                          ),
+                        ]
+                      ),
+                    ],
+                  )
+                ]
+              )
+            )
+          ),
+          Flexible(
+            flex: 1,
+            fit: FlexFit.tight,
+            child: Row(
+              children: [
+                Expanded(
+                  child: CustomButton1(
+                    onPressed: () {
+                      Navigator.of(context).pushNamed(RoomPhotoGallery.routeName);
+                    },
+                    child: Text('Room Photos')
+                  )
+                ),
+              ],
+            ),
+          ),
+          Flexible(
+            flex: 1,
+            fit: FlexFit.tight,
+            child: Container(
+              margin: EdgeInsets.only(left: 3),
+              child: Align(
+                child: Text(
+                  'Notes',
+                  style: Theme.of(context).textTheme.display1
+                ), 
+                alignment: Alignment.centerLeft
+              )
+            )
+          ),
+          Flexible(
+            flex: 4,
+            child: ListView.builder(
+              itemCount: notes.length,
+              itemBuilder: (context, index) {
+                return Card(
+                  child: ListTile(
+                    title: Text('${notes[index].description}'),
+                    onTap: () {
+                      Navigator.of(context).pushNamed(RoomDetail.routeName);
+                    },
+                  ),
+                );
+              }
+            ),
+          ),
+        ]
+      ),
     );
+  }
+  }
+  void getFakeData(){
+  room = widget.room;
+  notes.add(RoomNote(id: 0, description: 'Painting Cabinets'));
+  notes.add(RoomNote(id: 1, description: 'De-grease walls'));
+  notes.add(RoomNote(id: 2, description: 'Hard-to-reach areas'));
+  notes.add(RoomNote(id: 3, description: 'Lots of masking'));
+  notes.add(RoomNote(id: 4, description: 'Minor drywall damage'));
   }
 }
